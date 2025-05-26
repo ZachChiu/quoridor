5a1aa58e845f83cd3c628d599e8a55f4ca9f0935
Zach <Zach>
2025-05-27 00:34:54 +0800

    chore: 新增 tailwind 的 eslint 與 prettier 並調整部分設定檔

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import tailwind from "eslint-plugin-tailwindcss";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...tailwind.configs["flat/recommended"],
+  ...compat.config({
+    extends: ['next', 'prettier'],
+  }),
+  {  
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  }
 ];
 
 export default eslintConfig;
