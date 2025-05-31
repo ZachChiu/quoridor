9a78d088a88ffcb198d5944d46410e77915170cf
Zach <Zach>
2025-06-01 02:16:23 +0800

    feat: 調整首頁樣式

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,60 @@
 'use client'
 
 import { useRouter } from "next/navigation";
-
+import SectionShadow from "./components/sectionShadow";
 
 
 export default function Home() {
   const router = useRouter();
 
-  const handleStartGame = () => {
+  const handleStartLocalGame = () => {
     router.push("/play");
+  };
+
+  const handleStartConnectGame = () => {
+    alert('還在準備中，請稍後再試');
   };
 
   return (
     <div className="flex min-h-screen items-center justify-center gap-16 overflow-hidden font-[family-name:var(--font-geist-sans)]">
-      <main className="row-start-2 flex h-full flex-1 flex-col items-center justify-center gap-8 ">
-        <h1 className="text-xl">圍牆圍棋 QUORIDOR</h1>
-        <div className="flex flex-col gap-4">
-          <button type="button" onClick={handleStartGame}>開始遊戲</button>
-          <button>加入遊戲</button>
+      <main className="row-start-2 flex h-full flex-1 flex-col items-center justify-center gap-8">
+        <div className="size-[90dvw] md:size-[90dvh]">
+          <SectionShadow>
+            <div className="relative flex size-full flex-col items-center justify-center gap-8 rounded-xl border-4 border-gray-900 bg-tertiary p-8 text-xl md:gap-12">
+              <h1 className="text-center text-3xl font-bold md:text-5xl">圍牆圍棋 QUORIDOR</h1>
+              <div className="flex flex-col gap-4">
+                <SectionShadow>
+                  <button
+                    type="button"
+                    className="relative w-full rounded-xl border-4 border-gray-900 bg-tertiary-500 p-4 text-xl hover:-translate-x-0.5 hover:-translate-y-0.5 focus:translate-x-1 focus:translate-y-1"
+                    onClick={handleStartLocalGame}
+                  >
+                    本機對戰 1 VS 1
+                  </button>
+                </SectionShadow>
+                <SectionShadow>
+                  <button
+                    type="button"
+                    className="relative w-full rounded-xl border-4 border-gray-900 bg-tertiary-400 p-4 text-xl hover:-translate-x-0.5 hover:-translate-y-0.5 focus:translate-x-1 focus:translate-y-1"
+                    onClick={handleStartConnectGame}
+                  >
+                    連線對戰 1 VS 1
+                  </button>
+                </SectionShadow>
+                {/* <SectionShadow>
+                  <button
+                    type="button"
+                    className="relative w-full rounded-xl border-4 border-gray-900 bg-[#ffe1be] p-4 text-xl hover:-translate-x-0.5 hover:-translate-y-0.5 focus:translate-x-1 focus:translate-y-1"
+                    onClick={handleStartGame}
+                  >
+                    加入遊戲
+                  </button>
+                </SectionShadow> */}
+              </div>
+            </div>
+          </SectionShadow>
         </div>
-       </main>
+      </main>
     </div>
   );
 }
